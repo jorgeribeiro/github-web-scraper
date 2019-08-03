3831ef0abef293166181ea112705d9b8a6a8eef2
Jorge Ribeiro <joorgemelo@gmail.com>
2019-08-03 19:33:59 -0300

    Iniciando implementacao do scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,10 +1,25 @@
-import repositories_reader
-import repositories_requester
+# -*- coding: utf-8 -*-
 
+from repositories_reader import read_repositories_file
+from repositories_requester import request_repo
+
+import requests
 from bs4 import BeautifulSoup
 
-def scraper():
-	repos = repositories_reader.read_repositories_file()
-	print(repos)
+def scrap_table():
+	repos = read_repositories_file()
+	response = request_repo(repos[0])
 
-scraper()	
+	soup = BeautifulSoup(response.text, 'html.parser')
+	a = soup.findAll('a', {'class': 'js-navigation-open'})
+	for i in a:
+		if len(i['href']) > 0:
+			print(i['href'])
+
+# 1. Carregar lista de repositórios
+repos = read_repositories_file()
+
+# 2. Realizar operações em cada repositório
+for r in repos:
+	response = request_repo(r)
+	soup = BeautifulSoup(response.text, 'html.parser')
