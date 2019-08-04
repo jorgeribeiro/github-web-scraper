819499c7c77920adcd5c526693330f860befdbc2
Jorge Ribeiro <joorgemelo@gmail.com>
2019-08-04 08:07:56 -0300

    Correçao da build [2]

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='github web scraper',
-      version='0.0.1',
-      description='Github Web Scraper',
-      author='',
-      author_email='',
-      url='',
-      packages=find_packages(),
-     )
+setup(
+	name='github web scraper',
+	version='0.0.1',
+	description='Web Scraping Github Repos',
+	author='Jorge Ribeiro',
+	author_email='joorgemelo@gmail.com',
+	url='https://github.com/jorgimello/github-web-scraper',
+	packages=find_packages(),
+)
