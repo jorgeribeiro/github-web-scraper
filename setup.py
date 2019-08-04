6be511e2bdf0d112fb7b6107f24ab77d3054ae38
Jorge Ribeiro <joorgemelo@gmail.com>
2019-08-04 08:04:48 -0300

    Tentando corrigir erro na build

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='github web scraper',
+      version='0.0.1',
+      description='Github Web Scraper',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
