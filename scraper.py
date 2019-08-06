fbf9924e189f9d6fa26a3a6c1441ad28696762fd
Jorge Ribeiro <joorgemelo@gmail.com>
2019-08-05 23:36:19 -0300

    Possivel encontrar texto de linhas e tamanho do arquivo

--- scraper.py
+++ scraper.py
@@ -24,21 +24,26 @@ def extract_folder_content(url):
 	soup = BeautifulSoup(response.text, 'html.parser')
 	return soup.tbody
 
+FILE_CLASS_FINDER = 'text-mono f6 flex-auto pr-3 flex-order-2 flex-md-order-1 mt-2 mt-md-0'
 def extract_file_content(url):
 	# Extrai conteúdo de um arquivo
-	# TODO: finalizar web scraping de arquivo (linhas e bytes)
-	pass
+	# TODO retornar apenas quantidade de linhas e tamanho do arquivo
+	response = request_url(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	return soup.find('div', class_=FILE_CLASS_FINDER).get_text().splitlines()
+
+# extract_file_content('jorgimello/github-web-scraper/blob/master/repositories.txt')
 
 PATH_TO_FOLDERS = '/tree/master'
 PATH_TO_FILES = '/blob/master'
 # 2. Realizar operações em cada repositório
-	# Operações: ler linhas, gerar árvore e salvar arquivo .txt
+# Operações: ler linhas, gerar árvore e salvar arquivo .txt
 for r in repos:
 	if not is_valid_repository(r):
 		continue
 	project_root_links = extract_folder_content(r)
 	# Se lista não vazia, repositório existe
-	if project_root_links:
+	if project_root_links:		
 		folders = project_root_links.find_all(href=re.compile(r + PATH_TO_FOLDERS))
 		files = project_root_links.find_all(href=re.compile(r + PATH_TO_FILES))
 		for i in folders:
