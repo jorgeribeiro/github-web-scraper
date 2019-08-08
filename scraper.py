eaa372c792c34011eba01f5e342f400251abe4ce
Jorge Ribeiro <joorgemelo@gmail.com>
2019-08-07 22:37:06 -0300

    Algumas correcoes em nomes de metodos e variaveis

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 def pull_folder_content(url):
     # Extrai conteúdo de um diretório
-    # Retorna html do tbody da tabela que contém os itens do diretório
+    # Retorna soup.tbody da tabela que contém os itens do diretório
     response = request_url(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup.tbody
@@ -18,59 +18,55 @@ FILE_ELEMENT_FINDER = 'div'
 FILE_CLASS_FINDER = 'text-mono f6 flex-auto pr-3 flex-order-2 flex-md-order-1 mt-2 mt-md-0'
 def pull_file_content(url):
     # Extrai conteúdo de um arquivo
-    # Retorna html da div que contém informações do arquivo
+    # Retorna soup.div que contém informações do arquivo
     response = request_url(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup.find(FILE_ELEMENT_FINDER, class_=FILE_CLASS_FINDER)
 
 def retrieve_lines_and_bytes_from_file(url):
     # Extrai número de linhas e bytes de um arquivo
+    # Retorna linhas e bytes se a div for encontrada
+    # TODO: possível refatoramento => receber apenas texto em vez do link do arquivo
     div = pull_file_content(url)
-    div_text = [t.strip() for t in div.get_text().splitlines() if t.strip() != '']
-    l, b = div_text[0], div_text[1]
-    lines = [int(s) for s in l.split() if s.isdigit()][0]
-    bytes_ = calculate_bytes(b)
-    return lines, bytes_
+    if div:
+        div_text = [t.strip() for t in div.get_text().splitlines() if t.strip() != '']
+        l, b = div_text[0], div_text[1]
+        lines = [int(s) for s in l.split() if s.isdigit()][0]
+        bytes_ = calculate_bytes(b)
+        return lines, bytes_
+    else:
+        return -1
 
 PATH_TO_FOLDERS = '/tree/master'
 PATH_TO_FILES = '/blob/master'
-def separate_folders_and_files(folder_html):
-    # TODO: incluir testes
-    # Utiliza o método find_all do BealtifulSoup para diferenciar diretórios e arquivos
-    # Retorna html onde são encontrados os padrões de referência para diretório e arquivo
-    folders = folder_html.find_all(href=re.compile(PATH_TO_FOLDERS))
-    files = folder_html.find_all(href=re.compile(PATH_TO_FILES))
-    return folders, files
-
-def extract_content_href(repository_html):
-    # TODO: incluir testes
+def extract_hrefs(repository_content):
     # Extrai href de elementos que contenham links para diretórios e arquivos
     # Retorna lista com os hrefs encontrados
-    folders_html, files_html = separate_folders_and_files(repository_html)
+    folders_html = repository_content.find_all(href=re.compile(PATH_TO_FOLDERS))
+    files_html = repository_content.find_all(href=re.compile(PATH_TO_FILES))
     hrefs_to_folders = [html['href'] for html in folders_html]
     hrefs_to_files = [html['href'] for html in files_html]
     return hrefs_to_folders, hrefs_to_files
 
-def explore_repo(repository_html):
-    # TODO: continuar método
-    folders, files = extract_content_href(repository_html)
+def explore_repo(repository_content):
+    # TODO: continuar método e incluir testes
+    folders, files = extract_hrefs(repository_content)
     for f in folders:
         print(f)
     for f in files:
         print(f)
 
-# 1. Carregar lista de repositórios
 repo_names = read_repositories_file()
-# 2. Realizar exploração em cada repositório
-# Operações: ler linhas, gerar árvore e salvar arquivo .txt
+# 1. Carregar lista de repositórios
 for repo_name in repo_names:
-    if not is_valid_repository(repo_name): 
+    # 2. Realizar exploração em cada repositório
+    if not is_valid_repository(repo_name):
         continue
-    project_root_html = pull_folder_content(repo_name)
-    # Se html não vazio, repositório encontrado => realizar exploração
-    if project_root_html: 
-        explore_repo(project_root_html)
-        
+    repo_root = pull_folder_content(repo_name)
+    if repo_root:
+        # Se repo_root não nulo, repositório encontrado => realizar exploração
+        explore_repo(repo_root)
+
 # Ideia de como armazenar extensões, linhas e bytes:
 # Um dict pra armazenar número de linhas e bytes, e uma lista pra guardar as keys
 # files = {'js': {'lines': 9, 'bytes': 15}, 'txt': {'lines': 10, 'bytes': 15}, 'yml': {'lines': 20, 'bytes': 30}}
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 
-import pytest
+from bs4 import BeautifulSoup
 
-from scraper import pull_folder_content, pull_file_content, retrieve_lines_and_bytes_from_file, separate_folders_and_files, extract_content_href
+from scraper import pull_folder_content, pull_file_content, retrieve_lines_and_bytes_from_file, extract_hrefs
 
-def test_pull_valid_folder():
-    assert pull_folder_content('jorgimello/github-web-scraper') is not None
+def test_pull_on_valid_folder():
+    assert pull_folder_content('jorgimello/github-web-scraper')
 
-def test_pull_invalid_folder():
+def test_pull_on_invalid_folder():
     assert pull_folder_content('not-a-folder') is None
 
-def test_pull_valid_file():
-    assert pull_file_content('jorgimello/github-web-scraper/blob/master/repositories.txt') is not None
+def test_pull_on_valid_file():
+    assert pull_file_content('jorgimello/github-web-scraper/blob/master/repositories.txt')
 
-def test_pull_invalid_file():
+def test_pull_on_invalid_file():
     assert pull_file_content('jorgimello/github-web-scraper/tree/master/tests') is None
+
+def test_retrieve_lines_and_bytes_on_file():
+	assert retrieve_lines_and_bytes_from_file('jorgimello/github-web-scraper/blob/master/repositories.txt') != -1
+
+def test_retrieve_lines_and_bytes_on_non_file():
+	assert retrieve_lines_and_bytes_from_file('jorgimello') == -1
+
+def test_extract_content_on_valid_folder():
+	f = pull_folder_content('jorgimello/github-web-scraper')
+	assert extract_hrefs(f)
+
+def test_extract_content_on_empty_soup():
+	soup = BeautifulSoup('', 'html.parser')
+	assert extract_hrefs(soup)
